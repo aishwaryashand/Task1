6badd2c739100709a895248a3b2db7a6eeee2ec6
Aishwarya Shand <aishwarya14696@gmail.com>
2020-07-28 16:27:20 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
 import sys
 class Details:
 	def __init__(self,dob,roll_no):
-		print(dob)
+		dob = sys.argv[2]
+		roll_no = sys.argv[3]
+		module = __import__(module_name)
 Details(sys.argv[2],sys.argv[3])
