6e790e437fb0bb6687c48a86bac0c068a8882590
Aishwarya Shand <aishwarya14696@gmail.com>
2020-07-28 21:36:50 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
-class A:
-	def __init__(self,module_name):
-		module = __import__(module_name)
 
-A(sys.argv[1])
+class A:
+	def __init__(self, module_name, dob, roll_no):
+		module = __import__(module_name)
+		path,content = module.login(dob,roll_no)
+		print(path)
+		print(content)
+
+A(sys.argv[1],sys.argv[2],sys.argv[3])
+
