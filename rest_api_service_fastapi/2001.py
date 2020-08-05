3f9425558657313e108c02dfd61a029a5893833d
Aishwarya Shand <aishwarya14696@gmail.com>
2020-08-05 12:02:35 +0530

    Update 2001.py

--- rest_api_service_fastapi/2001.py
+++ rest_api_service_fastapi/2001.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+from datetime import date
+
+import constant
 # from upload import upload_file
 
 login_url = 'http://alliancebschool.org/studentportal/aced/results/php/results.php?page=btech6b1519jun18'
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0'}
 
 def login(params):
-	path = "/home/ashwarya.shand/Desktop/rest_api_service_fastapi/"+params.dob+"_"+params.roll_no+".html"
+	if not os.path.exists(constant.path_to_file+str(date.today())):
+		os.mkdir(str(date.today()))
+	path = constant.path_to_file+str(date.today())+"/"+params.dob+"_"+params.roll_no+".html"
 	values = {
 		'tregno': params.roll_no,
 		'tdob': params.dob,
